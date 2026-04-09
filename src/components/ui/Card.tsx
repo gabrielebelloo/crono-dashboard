ce588f2554d21461432cd4bad3f41fdfe2c6b76f
Gabriele <briello2001@gmail.com>
2026-04-10 00:54:16 +0200

    feat: add new SVG icons and enhance Card component with header features

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -1,10 +1,37 @@
 type CardProps = {
   children: React.ReactNode;
   className?: string;
+  headerTitle?: string;
+  headerAction?: string;
+  headerActionIcon?: React.FunctionComponent;
+  headerCounter?: number;
 };
 
-export default function Card({ children, className }: CardProps) {
+export default function Card({
+  children,
+  className,
+  headerTitle,
+  headerAction,
+  headerActionIcon: HeaderActionIcon,
+  headerCounter,
+}: CardProps) {
   return (
-    <div className={`bg-white rounded-xl border border-border p-6 ${className}`}>{children}</div>
+    <div className={`flex flex-col bg-white rounded-xl border border-border p-4 ${className}`}>
+      {!!headerTitle && (
+        <header className="flex justify-between items-center">
+          <div className="flex gap-2">
+            <div className="font-semibold">{headerTitle}</div>
+            {!!headerCounter && <div className="bg-yellow-500 w-[24px] h-[24px] rounded-[12px]">{headerCounter}</div>}  
+          </div>
+          {!!headerAction && (
+            <button className="flex justify between items-center gap-1 text-main text-sm cursor-pointer">
+              {headerAction}
+              {HeaderActionIcon && <HeaderActionIcon />}
+            </button>
+          )}
+        </header>
+      )}
+      {children}
+    </div>
   );
 }
--- src/pages/DashboardPage/DashboardPage.tsx
+++ src/pages/DashboardPage/DashboardPage.tsx
@@ -1,19 +1,30 @@
 import Card from "../../components/ui/Card";
+import ArrowIcon from "../../assets/arrow.svg?react";
+import EditIcon from "../../assets/edit.svg?react";
 
 export default function DashboardPage() {
   return (
     <div className="grid grid-cols-1 xl:grid-cols-12 gap-2">
-      <Card className="xl:col-span-4">Welcome</Card>
+      <Card className="xl:col-span-4">
+        <div className="py-4 px-[18px]">
+          <div className="text-[24px] font-bold">Welcome Alex,</div>
+          <div className="text-md text-gray">
+            Here’s your performance overview where you can track your daily and monthly KPIs
+          </div>
+        </div>
+      </Card>
 
-      <Card className="xl:col-span-4">Replies</Card>
+      <Card headerTitle="Replies" headerAction="Open inbox" headerActionIcon={ArrowIcon} className="xl:col-span-4">
+        test
+      </Card>
 
-      <Card className="xl:col-span-4 xl:row-span-2">May's Performance</Card>
+      <Card headerTitle="May's Performance" headerAction="Edit KPIs" headerActionIcon={EditIcon} className="xl:col-span-4 xl:row-span-2">Test</Card>
 
-      <Card className="xl:col-span-8">Tasks</Card>
+      <Card headerTitle="Tasks" className="xl:col-span-8">test</Card>
 
-      <Card className="xl:col-span-8">Signals</Card>
+      <Card headerTitle="Signals" headerCounter={12} className="xl:col-span-8">test</Card>
 
-      <Card className="xl:col-span-4">Onboarding</Card>
+      <Card headerTitle="Onboarding" className="xl:col-span-4">test</Card>
     </div>
   );
 }
